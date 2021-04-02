ed0b3b1ea78af3cf29d871cf62f38307bdeda7d0
TorchAblaze <tiffanygreathead@gmail.com>
2021-04-02 09:07:11 -0700

    Add body elements to index.html and utility log to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 // Business Logic
 
 // UI Logic
+$(document).ready(function () {
+  $("form#roboger").sumbit(function (event) {
+    event.preventDefault();
+    const userNum = $("#num-input").val();
+  });
+});
