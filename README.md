4533ea854669867cf63ccca6ef1b1ea4f2d0a29b
TorchAblaze <tiffanygreathead@gmail.com>
2021-04-02 09:32:03 -0700

    Add first completed group of tests

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 Describe: userNumber()
 
 Test: "It should take input from a user and return a number"
-Code: userNumber("12")
+Code: userNumber("12");
 Expected Output: 12
+
+Test: "If the user doesn't enter a number, return the string"
+Code: userNumber("Hello");
+Expected Output: Hello
+
+Describe: outputMessage()
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 // Business Logic
 
 // UI Logic
+function userNumber(userInput) {
+  numberRange = parseInt(userInput);
+  if (Number.isNaN(numberRange)) {
+    return userInput;
+  } else {
+    return numberRange;
+  }
+}
+
 $(document).ready(function () {
   $("form#roboger").sumbit(function (event) {
     event.preventDefault();
